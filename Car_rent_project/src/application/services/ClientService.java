da254144bd5377553071d54fc3f37b901947b04e
Johntarakay <istolpnikoff@gmail.com>
2025-04-07 21:23:46 +0300

    Implemented core functionality for the Car Rental project:
    - Added Models (Client, Car, Rental) with basic attributes and methods.
    - Developed Services (ClientService, CarService, RentalService) to
    handle business logic.
    - Created Repositories (ClientRepository, CarRepository,
    RentalRepository) for data management.
    - Integrated basic CRUD operations for Clients, Cars, and Rentals.
    - Implemented serialization for Client, Car, and Rental entities,
    allowing data to be saved to and loaded from files.
    - Structured the code to ensure clear separation of concerns between
    layers.

--- Car_rent_project/src/application/services/ClientService.java
+++ Car_rent_project/src/application/services/ClientService.java
@@ -1,5 +1,34 @@
 package application.services;
 
-public class ClientService {
+import models.Client;
+import repositories.ClientRepository;
 
+public class ClientService {
+    private ClientRepository clientRepository;
+    
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+    
+    public void createClient(Client client) {
+        // Валидируем данные клиента
+        if (ValidationService.validateClientData(client)) {
+            clientRepository.save(client);
+            System.out.println("Клиент создан: " + client);
+        } else {
+            System.out.println("Неверные данные клиента!");
+        }
+    }
+    
+    public Client getClientById(int clientId) {
+        return clientRepository.getClientById(clientId);
+    }
+    
+    public void updateClient(Client client) {
+        clientRepository.updateClient(client);
+    }
+    
+    public void deleteClient(int clientId) {
+        clientRepository.deleteClient(clientId);
+    }
 }
