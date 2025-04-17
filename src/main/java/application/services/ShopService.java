879c41526224770b344ac8c408d16a9dc515cd33
anat <anatignatovich@gmail.com>
2025-04-17 22:55:36 +0300

    17.04.25 night ch add manager design, form, controller empty files add enter_auth design, form, controller empty files add shopServ methods for item-lists

--- src/main/java/application/services/ShopService.java
+++ src/main/java/application/services/ShopService.java
@@ -1,7 +1,5 @@
 package application.services;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -10,24 +8,17 @@ import application.repositories.ShopRepository;
 
 public class ShopService {
 	private ShopRepository shopRepository;
-	
-	
-	private static List<String> citiesList = Arrays.asList("Cities","Rishon LeZion", "Tel Aviv", "Ramat Gan");
-	private static List<String> pointsAll = Arrays.asList("All");
-	private static List<String> pointsRL = Arrays.asList("All shops RL", "Shop 1", "Shop 2", "Shop3");
-	private static List<String> pointsTA = Arrays.asList("All shops TA","Shop 4", "Shop 5");
-	private static List<String> pointsRG = Arrays.asList("All shops RG","Shop 6", "Shop 7", "Shop8");
-	
-	public List<String> getAllShopsCitiesList(){
-		
+
+	public List<String> getAllShopsCitiesList() {
 		return shopRepository.getShops().stream().map(Shop::getCity).collect(Collectors.toList());
 	}
-	
-	public List<String> getAllShopsNames(){
+
+	public List<String> getAllShopsNames() {
 		return shopRepository.getShops().stream().map(Shop::getName).collect(Collectors.toList());
 	}
-	
-	public List<String> getShopNamesByCity
-	
-	
+
+	public List<String> getShopNamesByCity(String city) {
+		return shopRepository.getShops().stream().filter(t -> t.getCity().equalsIgnoreCase(city)).map(Shop::getName)
+				.collect(Collectors.toList());
+	}
 }
