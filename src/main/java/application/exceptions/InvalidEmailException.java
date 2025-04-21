49244106eb54eed6fb3a7a0d49fb08c43bb5b94d
Johntarakay <istolpnikoff@gmail.com>
2025-04-21 16:53:22 +0300

    Create InvalidEmailException.java
    Added new exception for email

---
+++ src/main/java/application/exceptions/InvalidEmailException.java
@@ -0,0 +1,10 @@
+package application.exceptions;
+
+/**
+ * Custom exception thrown when an email address is invalid.
+ */
+public class InvalidEmailException extends Exception {
+    public InvalidEmailException(String message) {
+        super(message);
+    }
+}
